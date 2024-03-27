41ccf0b78483f3f7e0530751b636f1b93b6992ac
#81279988 <81279988+aswin-natraj@users.noreply.github.com>
2024-03-27 15:45:17 +0530

    commit
    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+const APP_STARTED_LOG = `
+==== Ywaiter API 🚀💥 =====
+PORT: 4332
+ENVIRONMENT: ${process.env.NODE_ENV}
+STARTED AT: ${new Date()}
+Listening...
+=======\n`;
+
+app.get("/", (req, res) => {
+  res.send("hii");
+});
+
+app.listen(4332, (err) => {
+  if (err) console.log("err", err);
+
+  console.log(APP_STARTED_LOG);
+});
